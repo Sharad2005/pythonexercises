7f18e6ac8be016b02f8a7b39bfc8830160a926ac
Sharad Siddanagoudar <sharadsiddanagoudar@gmail.com>
2020-12-20 16:22:26 +0530

    corrected the spelling

--- exercise1/fullname2inital.py
+++ exercise1/fullname2inital.py
@@ -1,8 +1,8 @@
 print("This program will take your full name as input and gives back your initial")
 your_first_name=input("Please enter your first name:")
-your_middel_name=input("Please enter your middle name:")
+your_middle_name=input("Please enter your middle name:")
 your_last_name=input("Please enter your last name:")
 
-inital=your_first_name[0]+"."+your_middel_name[0]+"."+your_last_name[0]+"."
+inital=your_first_name[0]+"."+your_middle_name[0]+"."+your_last_name[0]+"."
 
 print("Your initial is",inital)
