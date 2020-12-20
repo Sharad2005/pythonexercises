e9b8de2ea7331dd86b1805c33b8b5ad9b24597d1
Sharad Siddanagoudar <sharadsiddanagoudar@gmail.com>
2020-12-20 09:23:25 +0530

    this is my first commit,exercise1,this example converts full name to inital

---
+++ exercise1/fullname2inital.py
@@ -0,0 +1,7 @@
+print("this program will show your inital")
+your_first_name=input("please enter your first name:")
+your_middel_name=input("please enter your middle name:")
+your_last_name=input("please enter your last name:")
+inital=your_first_name[0]+"."+your_middel_name[0]+"."+your_last_name[0]+"."
+
+print("your initial is",inital)
