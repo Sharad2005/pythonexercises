72a356144935cb00bc0e429c3212c4cc8b695ff6
Sharad Siddanagoudar <sharadsiddanagoudar@gmail.com>
2020-12-20 09:35:46 +0530

    Corrected the user suggesting texts

--- exercise1/fullname2inital.py
+++ exercise1/fullname2inital.py
@@ -1,7 +1,8 @@
-print("this program will show your inital")
-your_first_name=input("please enter your first name:")
-your_middel_name=input("please enter your middle name:")
-your_last_name=input("please enter your last name:")
+print("This program will take your full name as input and gives back your initial")
+your_first_name=input("Please enter your first name:")
+your_middel_name=input("Please enter your middle name:")
+your_last_name=input("Please enter your last name:")
+
 inital=your_first_name[0]+"."+your_middel_name[0]+"."+your_last_name[0]+"."
 
-print("your initial is",inital)
+print("Your initial is",inital)
