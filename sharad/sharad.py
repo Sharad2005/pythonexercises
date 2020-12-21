b77b319cffa8c5ab8ed702f33dd36accaf9ea763
Sharad Siddanagoudar <sharadsiddanagoudar@gmail.com>
2020-12-21 15:05:03 +0530

    new exercise

--- sharad/sharad.py
+++ sharad/sharad.py
@@ -1,1 +1,8 @@
-print("blah blah")
+import math
+num1=float(input("Please enter the number which you want the solution:"))
+area=math.pi * (num1 ** 2)
+circumference=math.pi*2*num1
+
+
+print("area=", area)
+print("circumference=" ,circumference)
