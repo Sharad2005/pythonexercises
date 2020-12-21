8264d33c463bda1eeda4dd7bd6f66be374ddee72
Sharad Siddanagoudar <sharadsiddanagoudar@gmail.com>
2020-12-21 20:32:54 +0530

    exercise3

---
+++ exercise3/exercise3.py
@@ -0,0 +1,7 @@
+date=float(input("please enter your date of birth:"))
+month=int(input("please enter your month you have born:"))
+year=float(input("please enter your year you have born:"))
+dateofbirth="years","Jan","feb","mar","april","may","june","july","aug","sep","oct","nov","dec"
+dateofbirth=["years","Jan","feb","mar","april","may","june","july","aug","sep","oct","nov","dec"]
+mouth1=dateofbirth[month]
+print(mouth1)
