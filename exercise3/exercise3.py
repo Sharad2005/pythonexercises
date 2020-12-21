81a447aad932e4a9bfa6d7bf77ce031527e7eaa8
Sharad Siddanagoudar <sharadsiddanagoudar@gmail.com>
2020-12-21 21:04:52 +0530

    practice

--- exercise3/exercise3.py
+++ exercise3/exercise3.py
@@ -1,7 +1,10 @@
 date=float(input("please enter your date of birth:"))
 month=int(input("please enter your month you have born:"))
 year=float(input("please enter your year you have born:"))
+name1=input("please entery your name because we are adding it to our list:")
 dateofbirth="years","Jan","feb","mar","april","may","june","july","aug","sep","oct","nov","dec"
 dateofbirth=["years","Jan","feb","mar","april","may","june","july","aug","sep","oct","nov","dec"]
+name="jhon","mrbeast","Tefu",
+name=["jhon","mrbeast","Tefu"]
 mouth1=dateofbirth[month]
-print(mouth1)
+print("you born month is ",mouth1)
