c9526272ea961517fb8353a198870c94129436b3
Sharad Siddanagoudar <sharadsiddanagoudar@gmail.com>
2020-12-26 12:40:30 +0530

    practices

---
+++ exercise2/exercise.py
@@ -0,0 +1,4 @@
+person={"name":"sharad","gender":"male","age":"15","address":"siddanagoudar","phone":"12345678979"}
+information=(input("please enter your info you want to know:"))
+person[information]
+print(person[information])
