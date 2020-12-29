6c29c045f096de307315a4a3b15f1816a7867d89
Sharad Siddanagoudar <sharadsiddanagoudar@gmail.com>
2020-12-29 12:46:42 +0530

    exercise4

---
+++ exercise4/exercise4.py
@@ -0,0 +1,7 @@
+age=float (input("please enter your age: "))
+if (age>15):
+    print("you are elder than me")
+elif(age<15):
+    print("you are younger than me")
+else:
+    print("we both are same age")
